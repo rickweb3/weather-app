f9c76282d3921bc9ce825fe24995bb06352459c7
Henrique Prado <henrique.mxnero@gmail.com>
2021-07-21 12:41:24 -0300

    Implementação do ângulo do vento utilizando a informação fornecida pela api e correção na exibição da div resultado, para ficar de forma íntegra (no caso exibir o que é para ser exibido de forma correta)

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
     let input = document.querySelector('#searchInput').value;
 
     if (input !== '') {
+        clearInfo();
         showWarning('Carregando...');
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=d06cdb298fafc83c520d5ab677fc477e&units=metric&lang=pt_br`;
 
         let results = await fetch(url);
         let json = await results.json();
-
 
         if (json.cod === 200) {
             showInfo({
@@ -22,9 +22,12 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
                 windAngle: json.wind.deg,
             });
         } else {
+            clearInfo();
             showWarning('Não encontramos esta localização.');
         }
 
+    } else {
+        clearInfo();
     }
 
 });
@@ -33,14 +36,21 @@ document.querySelector('.busca').addEventListener('submit', async (event) => {
 function showInfo(json) {
     showWarning('');
 
-    document.querySelector('.resultado').style.display = 'block';
-
     document.querySelector('.titulo').innerHTML = `${json.name}, ${json.country}`;
     document.querySelector('.tempInfo').innerHTML = `${json.temp} <sup>ºC</sup>`;
     document.querySelector('.ventoInfo').innerHTML = `${json.windSpeed} <span>km/h</span>`;
 
     document.querySelector('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
 
+    document.querySelector('.ventoPonto').style.transform = `rotate(${json.windAngle - 90}deg)`;
+
+    document.querySelector('.resultado').style.display = 'block';
+}
+
+
+function clearInfo() {
+    showWarning('');
+    document.querySelector('.resultado').style.display = 'none';
 }
 
 
